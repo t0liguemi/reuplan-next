104e6c805c11de1bf4cbc866486a12a73c63f8bc
toliguemi <toliguemi94@gmail.com>
2024-09-13 05:01:52 -0300

    deleted leftover dict file

--- src/app/(clientfacing)/[locale]/dictionaries.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only'
- 
-const dictionaries = {
-  en: () => import('~/dictionaries/en.json').then((module) => module.default),
-  es: () => import('~/dictionaries/es.json').then((module) => module.default),
-}
- 
-export const getDictionary = async (locale: string) => dictionaries[locale]()
