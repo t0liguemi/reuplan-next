a124c699ea07bd655b34e836efe24161eb1cce4e
toliguemi <toliguemi94@gmail.com>
2024-09-13 05:19:25 -0300

    ???

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,12 +1,18 @@
-import {notFound} from 'next/navigation';
-import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
- 
-export default getRequestConfig(async ({locale}) => {
+import { notFound } from "next/navigation";
+import { getRequestConfig } from "next-intl/server";
+import { routing, type Locale } from "./routing";
+
+export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as any)) notFound();
- 
+  if (!routing.locales.includes(locale as Locale)) notFound();
+
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    messages: (
+      await(locale === "en"
+      ? import("../messages/en.json")
+      : import(`../messages/${locale}.json`)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    )).default,
   };
 });
